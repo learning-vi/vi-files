63f7b5cfbfab7a18df14ccb9b7a0d383d3fcc691
The vi Guys <learning.vi.and.vim@gmail.com>
1993-07-24 16:25:12 +0000

    Add stack.ex

---
+++ amaze-your-friends/docs/vifs/stack.ex
@@ -0,0 +1,8 @@
+"
+" edit-file stack ( #+:push, #-:pop, #=:list, #:pop, #:push & tag )
+"
+map	#+	:1,.w !exec vifs push %
+map	#-	:!exec vifs pop:so ~/tmp/vifs_pop.ex
+map	#=	:!exec vifs
+map	#	:1,.w !exec vifs push %
+map	#	:!exec vifs pop:so ~/tmp/vifs_pop.ex
